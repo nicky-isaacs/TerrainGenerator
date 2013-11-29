8df02dd7b1bef153a12e4475d0a40df7db87aa27
Nick Isaacs <nicholas.isaacs@colorado.edu>
2013-11-29 11:52:31 -0700

    features work

--- features/step_definitions/login_step.rb
+++ features/step_definitions/login_step.rb
@@ -2,15 +2,15 @@
  
 
 When /^I visit the login screen$/ do
-  visit ui_url '/'
+  visit ui_url '/users/sign_in'
 end
 
 And /^I enter username "(.*?)"$/ do |username|
-  fill_in('username', :with => username)
+  fill_in("#user_email", :with => username)
 end
 
 And /^I enter password "(.*?)"$/ do |password|
-  fill_in('password', :with => password)
+  fill_in("#user_password", :with => password)
 end
 
 Then /^I should be logged in$/ do
