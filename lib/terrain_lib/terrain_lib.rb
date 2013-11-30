8cf7c3a854e7dcc90aaeda31357c5dcaec43a207
Nick Isaacs <nisaacs@splickit.com>
2013-11-30 18:32:36 +0000

    fixed dependancies

--- lib/terrain_lib/terrain_lib.rb
+++ lib/terrain_lib/terrain_lib.rb
@@ -1,2 +1,6 @@
-require 'terrain/src/component'
-require 'terrain/src/height_map'
+src_path = File.join( File.dirname(__FILE__), 'src')
+
+Dir.entries(src_path).each do |f|
+	f = File.join(src_path, f)
+	require f if f.include? '.rb'
+end
