3db751b38494bdb7b3401103774b3fbbca29ee18
Nick Isaacs <nicholas.isaacs@colorado.edu>
2013-12-03 20:21:01 -0700

    require lib

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+require File.join(File.dirname(__FILE__), '..', 'lib', 'terrain_lib', 'terrain_lib.rb')
+
 module TerainGeneration
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
