34d2853f45e11ab1d3dacc21e9f5c4288972804d
Mitchell Wolfe <mitchell.wolfe@colorado.edu>
2013-12-04 20:39:19 -0700

    user sign up attempted fix 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ end
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  
   protect_from_forgery with: :exception
 
   ADMIN_USER_EMAILS = %w(
