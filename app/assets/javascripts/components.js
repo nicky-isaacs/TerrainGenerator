cb9d8ebb99e75a87e71adcbb9ed94fa3aff50dc1
Nick Isaacs <nicholas.isaacs@colorado.edu>
2013-12-04 19:29:27 -0700

    reworking frontend

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -2,6 +2,57 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://coffeescript.org/
 
+
+var component_select_class = "component_selection";
+var component_variable_select_class = "component_variable_selection";
+
+createFieldBasedOnType = function(type){
+    var input_field_names = getComponentInputOutputMap()[type.toLowerCase()][0];
+    var name_field = $('<input class=\"component_name\"></input>');
+
+    var input_field_options=[];
+    $('input_field_names').each(function(index){
+        var option_tag_str = '<option value=\"' + this + '\">' + this + '</option>';
+        input_field_options.push( $(option_tag_str) );
+    });
+
+
+    var input_field_tags=[];
+    $(input_field_names).each(function(index){
+        var wrapper = $('<div class="input_field_wrapper"></div>');
+
+        var component_select_str = '<select class=\"' + component_select_class + '\"></select>'
+        var src_component_select = $(component_select_str);
+        var src_component_select_with_options = appendToSelect(src_component_select, input_field_options);
+
+
+        input_field_tags.push(thing);
+    });
+}
+
+populateVariableSelect = function(variable_select_to_mod, component_type){
+    var option_list = getComponentInputOutputMap()[component_type.toLowerCase()][0];
+    appendToSelect(variable_select_to_mod, option_list);
+}
+
+componentInputClickHandler = function(){
+    $('.' + component_select_class).change(function(component_name){
+        populateVariableSelect(this, component_name);
+    });
+}
+
+appendToSelect = function(select, options){
+    $(options).each(function(index){
+        select.append(this);
+    });
+
+    return select;
+}
+
+existingComponentNames = function(){
+    //not implimented
+}
+
 cleanInputs = function(required, passed){
     console.log("Cleaning: " + passed.toString() );
     console.log("Required: " + required );
