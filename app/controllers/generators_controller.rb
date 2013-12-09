efc919e3a4d882d2958375d819e8491add61c14a
Nick Isaacs <nicholas.isaacs@colorado.edu>
2013-12-09 10:29:50 -0700

    add new test object

--- app/controllers/generators_controller.rb
+++ app/controllers/generators_controller.rb
@@ -69,7 +69,7 @@ class GeneratorsController < ApplicationController
 
   # =Renders the preview partial
   def preview
-    if (Rails.env.development? || is_admin?) && params[:id].nil?
+    if (Rails.env.development? || is_admin?) && (params[:id].nil? || params[:show_test_object])
       @obj_path = "/test/test.obj"
       flash[:alert] = "Displaying the test object"
     elsif !@obj_path
