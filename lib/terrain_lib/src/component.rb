cfb3873f42dfed02f88c600e2b8be4a02fa41d92
Nick Isaacs <nicholas.isaacs@colorado.edu>
2013-12-14 18:32:37 -0700

    test gen and indents and refector

--- lib/terrain_lib/src/component.rb
+++ lib/terrain_lib/src/component.rb
@@ -54,7 +54,7 @@ module TerrainLib
 
   class Component
     attr_accessor :inputs
-  
+
     def initialize( params={} )
       @outputs = params[:outputs]
       @outputs = nil if @outputs.nil? || @outputs.empty?
@@ -63,97 +63,97 @@ module TerrainLib
       @type = params[:type]
       if @type == nil then @type = "value" end
     end
-    
+
     def self.convert(metadata)
-        components = {}
-        metadata.each do |k,v|
-            if v.has_key?("inputs") then
-                v[:inputs] = v["inputs"]
-                v["inputs"] = nil
-            end
-            if v.has_key?("outputs") then
-                v[:outputs] = v["outputs"]
-                v["outputs"] = nil
-            end
-            if v.has_key?("type") then
-                v[:type] = v["type"]
-                v["type"] = nil
-            end
-            components[k] = self.new(v)
+      components = {}
+      metadata.each do |k,v|
+        if v.has_key?("inputs") then
+          v[:inputs] = v["inputs"]
+          v["inputs"] = nil
         end
-        
-        components.each do |k,v|
-            v.inputs().each do |i,j|
-                if j.first != "sampler" then
-                    j[0] = components[j.first]
-                end
-            end
+        if v.has_key?("outputs") then
+          v[:outputs] = v["outputs"]
+          v["outputs"] = nil
         end
-        
-        return components["result"]
+        if v.has_key?("type") then
+          v[:type] = v["type"]
+          v["type"] = nil
+        end
+        components[k] = self.new(v)
+      end
+
+      components.each do |k,v|
+        v.inputs().each do |i,j|
+          if j.first != "sampler" then
+            j[0] = components[j.first]
+          end
+        end
+      end
+
+      return components["result"]
     end
 
     def self.generate(metadata)
-        return self.convert(metadata).generate()
+      return self.convert(metadata).generate()
     end
-    
+
     def self.isValidHash?(hash)
-        unless hash.class.name == "Hash" and hash.has_key?("result") and hash["result"].has_key?("type") and hash["result"]["type"] == "result" then return [false, "hash has no valid result component", 0] end
-        hash.each do |k,v|
-            a,b = self.isValidComponent?(k, v, hash)
-            if not a then return [false, b] end
-        end
-        return [true, nil]
+      return [false, "hash has no valid result component"] unless hash.class.name == "Hash" && hash.has_key?("result") && hash["result"].has_key?("type") && hash["result"]["type"] == "result"
+      hash.each do |k,v|
+        a,b = self.isValidComponent?(k, v, hash)
+        if not a then return [false, b] end
+      end
+      return [true, nil]
     end
-    
+
     def self.isValidComponent?(k, v, hash, prev = [].to_set)
-        if prev.include?(k) then return [true, nil] end
-        prev.add(k)
-        if v.class.name != "Hash" then return [false, "component #{k} is not a hash!"] end
-        if not v.has_key?("type") then return [false, "component #{k} has no type!"] end
-        if v["type"].class.name != "String" then return [false, "component #{k}'s type field must be a string!"] end
-        if not TerrainLib::ComponentTypesLookup::lookup_table.has_key?(v["type"]) then return [false, "component #{k} has an invalid type!"] end
-        if v.has_key?("outputs") then
-            out = v["outputs"]
-            if out.class.name != "Hash" then return[false, "component #{k}'s outputs are not a hash!"] end
-            out.each do |l,w|
-                if l.class.name != "String" then return [false, "component #{k}'s outputs include non-string keys!"] end
-                if not w.respond_to?(:to_f) then return [false, "component #{k}'s outputs include non-convertible-to-float values"] end
-            end
+      if prev.include?(k) then return [true, nil] end
+      prev.add(k)
+      if v.class.name != "Hash" then return [false, "component #{k} is not a hash!"] end
+      if not v.has_key?("type") then return [false, "component #{k} has no type!"] end
+      if v["type"].class.name != "String" then return [false, "component #{k}'s type field must be a string!"] end
+      if not TerrainLib::ComponentTypesLookup::lookup_table.has_key?(v["type"]) then return [false, "component #{k} has an invalid type!"] end
+      if v.has_key?("outputs") then
+        out = v["outputs"]
+        if out.class.name != "Hash" then return[false, "component #{k}'s outputs are not a hash!"] end
+        out.each do |l,w|
+          if l.class.name != "String" then return [false, "component #{k}'s outputs include non-string keys!"] end
+          if not w.respond_to?(:to_f) then return [false, "component #{k}'s outputs include non-convertible-to-float values"] end
         end
-        if v.has_key?("inputs") then
-            inp = v["inputs"]
-            if inp.class.name != "Hash" then return [false, "component #{k}'s inputs are not a hash!"] end
-            inp.each do |l,w|
-                if l.class.name != "String" then return [false, "component #{k}'s inputs include non-string keys!"] end
-                if w.class.name != "Array" then return [false, "component #{k}'s input #{l} must be an array!"] end
-                if w.first != "sampler" then
-                    if w.first.class.name != "String" or not hash.has_key?(w.first) then return [false, "component #{k}'s input #{l}'s first element must be the name of another component!"] end
-                    s,m = self.isValidComponent?(w.first, hash[w.first], hash, prev)
-                    if not s then return [false, m] end
-                    if w[-1].class.name != "String" or ((not hash[w.first].has_key?("outputs") or hash[w.first]["outputs"].class.name != "Hash" or not hash[w.first]["outputs"].has_key?(w[-1])) and (not TerrainLib::ComponentTypesLookup::lookup_table[hash[w.first]["type"]][-1].include?(w[-1]))) then return [false, "component #{k}'s input #{l}'s last element must be the name of an output of another component!"] end
-                end
-            end
+      end
+      if v.has_key?("inputs") then
+        inp = v["inputs"]
+        if inp.class.name != "Hash" then return [false, "component #{k}'s inputs are not a hash!"] end
+        inp.each do |l,w|
+          if l.class.name != "String" then return [false, "component #{k}'s inputs include non-string keys!"] end
+          if w.class.name != "Array" then return [false, "component #{k}'s input #{l} must be an array!"] end
+          if w.first != "sampler" then
+            if w.first.class.name != "String" or not hash.has_key?(w.first) then return [false, "component #{k}'s input #{l}'s first element must be the name of another component!"] end
+            s,m = self.isValidComponent?(w.first, hash[w.first], hash, prev)
+            if not s then return [false, m] end
+            if w[-1].class.name != "String" or ((not hash[w.first].has_key?("outputs") or hash[w.first]["outputs"].class.name != "Hash" or not hash[w.first]["outputs"].has_key?(w[-1])) and (not TerrainLib::ComponentTypesLookup::lookup_table[hash[w.first]["type"]][-1].include?(w[-1]))) then return [false, "component #{k}'s input #{l}'s last element must be the name of an output of another component!"] end
+          end
         end
-        return [true, nil]
+      end
+      return [true, nil]
     end
 
     def sample(coord)
-        @@sampler = coord
-        result = self.output
-        reset()
-        return result
+      @@sampler = coord
+      result = self.output
+      reset()
+      return result
     end
-    
+
     def reset()
-        if @type != "value" then @outputs = nil end
-        @inputs.each do |k,v|
-            if v.first != "sampler" and not v.first.nil? then
-                v.first.reset()
-            end
+      if @type != "value" then @outputs = nil end
+      @inputs.each do |k,v|
+        if v.first != "sampler" and not v.first.nil? then
+          v.first.reset()
         end
+      end
     end
-    
+
     def generate()
       filename = "lib/terrain_lib/out/" + Time.new.getutc.to_s + ".obj"
       File.open(filename, mode="w"){ |file|
@@ -164,6 +164,7 @@ module TerrainLib
           for y in 0..200
             # get the value at this position
             hgt = self.sample({"x" => x, "y" => y})["z"]
+            [x, y, hgt].each_with_index{ |t, i| raise "\nIndex #{i} was nil" if t.nil?}
             file.write("v #{(x * OBJ_SCALAR).to_s} #{(hgt * OBJ_SCALAR).to_s} #{(y * OBJ_SCALAR).to_s}\n")
             self.reset()
           end
@@ -197,7 +198,7 @@ module TerrainLib
         return src.first.output[src[-1]].to_f
       end
     end
-    
+
     def result()
       @outputs = {}
       @outputs["z"] = invalue("v")
@@ -291,37 +292,37 @@ module TerrainLib
     end
 
     def perlin()
-        @outputs = {}
-        sd = invalue("sd")
-        if sd.nan? then sd = 0 end
-        sd = sd.abs
-        if sd < 1 then sd = sd + 1 end
-        p = Perlin::Generator.new(sd.to_int, 1, 1, {:classic => true})
-        x = invalue("x")
-        y = invalue("y")
-        z = invalue("z")
-        if x.nan? then x = 98.1222 end
-        if y.nan? then y = 2877.211 end
-        if z.nan? then z = 12383.122 end
-        @outputs["v"] = p[x, y, z]
-        return @outputs
+      @outputs = {}
+      sd = invalue("sd")
+      if sd.nan? then sd = 0 end
+      sd = sd.abs
+      if sd < 1 then sd = sd + 1 end
+      p = Perlin::Generator.new(sd.to_int, 1, 1, {:classic => true})
+      x = invalue("x")
+      y = invalue("y")
+      z = invalue("z")
+      if x.nan? then x = 98.1222 end
+      if y.nan? then y = 2877.211 end
+      if z.nan? then z = 12383.122 end
+      @outputs["v"] = p[x, y, z]
+      return @outputs
     end
 
     def simplex()
-        @outputs = {}
-        sd = invalue("sd")
-        if sd.nan? then sd = 0 end
-        sd = sd.abs
-        if sd < 1 then sd = sd + 1 end
-        s = Perlin::Generator.new(sd.to_int, 1, 1, {:classic => false})
-        x = invalue("x")
-        y = invalue("y")
-        z = invalue("z")
-        if x.nan? then x = 98.1222 end
-        if y.nan? then y = 2877.211 end
-        if z.nan? then z = 12383.122 end
-        @outputs["v"] = s[x, y, z]
-        return @outputs
+      @outputs = {}
+      sd = invalue("sd")
+      if sd.nan? then sd = 0 end
+      sd = sd.abs
+      if sd < 1 then sd = sd + 1 end
+      s = Perlin::Generator.new(sd.to_int, 1, 1, {:classic => false})
+      x = invalue("x")
+      y = invalue("y")
+      z = invalue("z")
+      if x.nan? then x = 98.1222 end
+      if y.nan? then y = 2877.211 end
+      if z.nan? then z = 12383.122 end
+      @outputs["v"] = s[x, y, z]
+      return @outputs
     end
 
     def mag()
@@ -335,9 +336,9 @@ module TerrainLib
       w = invalue("w")
       if w.nan? then w = 0 end
       @outputs["m"] = Math.sqrt(x * x +
-                                y * y +
-                                z * z +
-                                w * w)
+                                    y * y +
+                                    z * z +
+                                    w * w)
       return @outputs
     end
 
@@ -352,9 +353,9 @@ module TerrainLib
       w = invalue("w")
       if w.nan? then w = 0 end
       @outputs["m"] = Math.sqrt(x * x +
-                                y * y +
-                                z * z +
-                                w * w)
+                                    y * y +
+                                    z * z +
+                                    w * w)
       @outputs["x"] = x / @outputs["m"]
       @outputs["y"] = y / @outputs["m"]
       @outputs["z"] = z / @outputs["m"]
@@ -375,97 +376,97 @@ module TerrainLib
       m = invalue("m")
       if m.nan? then m = 1 end
       @outputs["m"] = Math.sqrt((x * x) +
-                                (y * y) +
-                                (z * z) +
-                                (w * w))
+                                    (y * y) +
+                                    (z * z) +
+                                    (w * w))
       @outputs["x"] = x * m / @outputs["m"]
       @outputs["y"] = y * m / @outputs["m"]
       @outputs["z"] = z * m / @outputs["m"]
       @outputs["w"] = w * m / @outputs["m"]
       return @outputs
     end
-    
+
     def min()
-        @outputs = {}
-        x = invalue("x")
-        y = invalue("y")
-        z = invalue("z")
-        w = invalue("w")
-        a = invalue("a")
-        b = invalue("b")
-        c = invalue("c")
-        d = invalue("d")
-        if x.nan? then x = 0 end
-        if y.nan? then y = 0 end
-        if z.nan? then z = 0 end
-        if w.nan? then w = 0 end
-        if a.nan? then a = 0 end
-        if b.nan? then b = 0 end
-        if c.nan? then c = 0 end
-        if d.nan? then d = 0 end
-        @outputs["x"] = [x, a].min
-        @outputs["y"] = [y, b].min
-        @outputs["z"] = [z, c].min
-        @outputs["w"] = [w, d].min
-        return @outputs
+      @outputs = {}
+      x = invalue("x")
+      y = invalue("y")
+      z = invalue("z")
+      w = invalue("w")
+      a = invalue("a")
+      b = invalue("b")
+      c = invalue("c")
+      d = invalue("d")
+      if x.nan? then x = 0 end
+      if y.nan? then y = 0 end
+      if z.nan? then z = 0 end
+      if w.nan? then w = 0 end
+      if a.nan? then a = 0 end
+      if b.nan? then b = 0 end
+      if c.nan? then c = 0 end
+      if d.nan? then d = 0 end
+      @outputs["x"] = [x, a].min
+      @outputs["y"] = [y, b].min
+      @outputs["z"] = [z, c].min
+      @outputs["w"] = [w, d].min
+      return @outputs
     end
-    
+
     def max()
-        @outputs = {}
-        x = invalue("x")
-        y = invalue("y")
-        z = invalue("z")
-        w = invalue("w")
-        a = invalue("a")
-        b = invalue("b")
-        c = invalue("c")
-        d = invalue("d")
-        if x.nan? then x = 0 end
-        if y.nan? then y = 0 end
-        if z.nan? then z = 0 end
-        if w.nan? then w = 0 end
-        if a.nan? then a = 0 end
-        if b.nan? then b = 0 end
-        if c.nan? then c = 0 end
-        if d.nan? then d = 0 end
-        @outputs["x"] = [x, a].max
-        @outputs["y"] = [y, b].max
-        @outputs["z"] = [z, c].max
-        @outputs["w"] = [w, d].max
-        return @outputs
+      @outputs = {}
+      x = invalue("x")
+      y = invalue("y")
+      z = invalue("z")
+      w = invalue("w")
+      a = invalue("a")
+      b = invalue("b")
+      c = invalue("c")
+      d = invalue("d")
+      if x.nan? then x = 0 end
+      if y.nan? then y = 0 end
+      if z.nan? then z = 0 end
+      if w.nan? then w = 0 end
+      if a.nan? then a = 0 end
+      if b.nan? then b = 0 end
+      if c.nan? then c = 0 end
+      if d.nan? then d = 0 end
+      @outputs["x"] = [x, a].max
+      @outputs["y"] = [y, b].max
+      @outputs["z"] = [z, c].max
+      @outputs["w"] = [w, d].max
+      return @outputs
     end
-    
+
     def abs()
-        @outputs = {}
-        x = invalue("x")
-        y = invalue("y")
-        z = invalue("z")
-        w = invalue("w")
-        if x.nan? then x = 0 end
-        if y.nan? then y = 0 end
-        if z.nan? then z = 0 end
-        if w.nan? then w = 0 end
-        @outputs["x"] = x.abs
-        @outputs["y"] = y.abs
-        @outputs["z"] = z.abs
-        @outputs["w"] = w.abs
-        return @outputs
+      @outputs = {}
+      x = invalue("x")
+      y = invalue("y")
+      z = invalue("z")
+      w = invalue("w")
+      if x.nan? then x = 0 end
+      if y.nan? then y = 0 end
+      if z.nan? then z = 0 end
+      if w.nan? then w = 0 end
+      @outputs["x"] = x.abs
+      @outputs["y"] = y.abs
+      @outputs["z"] = z.abs
+      @outputs["w"] = w.abs
+      return @outputs
     end
-    
+
     def cmp()
-        @outputs = {}
-        if invalue("d") < 0 then
-            @outputs["x"] = invalue("a")
-            @outputs["y"] = invalue("b")
-            @outputs["z"] = invalue("c")
-            @outputs["w"] = invalue("d")
-        else
-            @outputs["x"] = invalue("x")
-            @outputs["y"] = invalue("y")
-            @outputs["z"] = invalue("z")
-            @outputs["w"] = invalue("w")
-        end
-        return @outputs
+      @outputs = {}
+      if invalue("d") < 0 then
+        @outputs["x"] = invalue("a")
+        @outputs["y"] = invalue("b")
+        @outputs["z"] = invalue("c")
+        @outputs["w"] = invalue("d")
+      else
+        @outputs["x"] = invalue("x")
+        @outputs["y"] = invalue("y")
+        @outputs["z"] = invalue("z")
+        @outputs["w"] = invalue("w")
+      end
+      return @outputs
     end
   end
 end
